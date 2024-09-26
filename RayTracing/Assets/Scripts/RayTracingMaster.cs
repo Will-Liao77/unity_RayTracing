ae48b07b23d804f3c0e6224fe432587f0d57618f
CW Liao <will030430@gmail.com>
2024-09-26 18:52:45 +0800

    backup\

--- RayTracing/Assets/Scripts/RayTracingMaster.cs
+++ RayTracing/Assets/Scripts/RayTracingMaster.cs
@@ -17,11 +17,15 @@ public class RayTracingMaster : MonoBehaviour
 
     private void Render(RenderTexture destination)
     {
+        // Make sure we have a current render target
         InitRenderTexture();
 
+        // Set the target and dispatch the compute shader
         RayTracingShader.SetTexture(0, "Result", _target);
+        // 8 is the number of threads per group
         RayTracingShader.Dispatch(0, Mathf.CeilToInt(_target.width / 8), Mathf.CeilToInt(_target.height / 8), 1);
 
+        // Blit the result texture to the screen
         Graphics.Blit(_target, destination);
     }
 
@@ -29,11 +33,13 @@ public class RayTracingMaster : MonoBehaviour
     {
         if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
         {
+            // Release render texture if we already have one
             if (_target != null)
             {
                 _target.Release();
             }
 
+            // Get a render target for Ray Tracing
             _target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
             _target.enableRandomWrite = true;
             _target.Create();
